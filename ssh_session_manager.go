ce00153fe1d1be78a21451b7ddc9f44bd2651683
Andrew <a-y.yushkov@outlook.com>
2022-12-29 19:48:29 +0800

    pull request to https://github.com/shenbowei/switch-ssh-go/pulls fix(RunAutoClean):修复死锁，与RunCmd #10 by jiangjiangjiangjiangjiangjiangjiang

--- ssh_session_manager.go
+++ ssh_session_manager.go
@@ -171,9 +171,9 @@ func (this *SessionManager) RunAutoClean() {
 			timeoutSessionIndex := this.getTimeoutSessionIndex()
 			this.sessionCacheLocker.Lock()
 			for _, sessionKey := range timeoutSessionIndex {
-				this.LockSession(sessionKey)
+				//this.LockSession(sessionKey)
 				delete(this.sessionCache, sessionKey)
-				this.UnlockSession(sessionKey)
+				//this.UnlockSession(sessionKey)
 			}
 			this.sessionCacheLocker.Unlock()
 			time.Sleep(30 * time.Second)
